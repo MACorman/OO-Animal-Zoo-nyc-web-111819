d17fb5f6c3cc7d99334d97dc27fdf986b3ef7a76
MACorman <madelinecorman@gmail.com>
2019-11-24 11:26:26 -0500

    completed zoo lab

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,37 @@
 class Zoo
 
+    attr_reader :name, :location
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def animals
+        Animal.all.select {|a| a.zoo == self }
+    end
+
+    def animal_species
+        (animals.map {|a| a.species}).uniq!
+    end
+
+    def find_by_species(species)
+        animals.select {|a| a.species == species }
+    end
+
+    def animal_nicknames
+        animals.map { |a| a.nickname }
+    end 
+
+    def self.find_by_location(location)
+        @@all.select {|z| z.location == location }
+    end 
+
 end
--- run.rb
+++ run.rb
@@ -2,8 +2,19 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
+z1 = Zoo.new("Hollywoo Zoo", "California")
+z2 = Zoo.new("Cheyenne Mountain Zoo", "Colorado")
 
-#Test your code here
+
+a1 = Animal.new("Horse", 500, "BoJack", z1)
+a2 = Animal.new("Cat", 10, "Princess Carolyn", z1)
+a3 = Animal.new("Dog", 75, "Mr. Peanut Butter", z1)
+a4 = Animal.new("Dog", 30, "Pickles", z1)
+a5 = Animal.new("Penguin", 25, "Pinky", z1)
+a6 = Animal.new("Lion", 400, "Adam", z2)
+a7 = Animal.new("Bear", 800, "Shane", z2)
+a8 = Animal.new("Zebra", 200, "Joe", z2)
+
 
 
 binding.pry
